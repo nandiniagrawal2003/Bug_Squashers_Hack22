7674754702a0bf79133f99fb1c6c2021bf3e9ae3
Sambhrant Tiwari <105651436+Cyclotron17@users.noreply.github.com>
2022-09-04 23:33:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/finadel/splashscreen.kt
@@ -0,0 +1,15 @@
+package com.example.finadel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this,MainActivity::class.java))},2000)
+        }
+    }
