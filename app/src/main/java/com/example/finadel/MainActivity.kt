a5db37926ddb16495c03f6a396749ace8333a6bb
Sambhrant Tiwari <105651436+Cyclotron17@users.noreply.github.com>
2022-09-11 17:21:58 +0530

    Initial commit

--- app/src/main/java/com/example/finadel/MainActivity.kt
+++ app/src/main/java/com/example/finadel/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.finadel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.findNavController
 import com.example.finadel.databinding.ActivityMainBinding
 
@@ -13,10 +15,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
-        val navController = navHostFragment.findNavController()
+        val navController = navHostFragment!!.findNavController()
         val popupMenu=PopupMenu(this,null)
         popupMenu.inflate(R.menu.bottom_nav)
         binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
-        navController.addOnDestinationChangedListener(object: On)
-    }
+        navController.addOnDestinationChangedListener(object  : NavController.OnDestinationChangedListener {
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title = when(destination.id){
+                    R.id.cartFragment -> "My Cart"
+                    R.id.moreFragment -> "My Dashboard"
+                    else -> "Finadel"
+                }
+            }
+
+            })
+        }
 }
